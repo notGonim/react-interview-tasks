1131a9be30bfc96f5a26341d15128a714b10705d
notGonim <mhmodgonim96@gmail.com>
2021-01-23 19:37:59 +0200

    Done : with last task

--- likebutton/src/App.js
+++ likebutton/src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
+import { React, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [likecolor, setlikecolor] = useState('gray')
+  const [dislikecolor, setDislikecolor] = useState('gray')
+  const [likeCounter, setLikeCounter] = useState(100)
+  const [dislikeCounter, setDislikeCounter] = useState(25)
+  const [liked, setLiked] = useState(false)
+  const [disliked, setDisliked] = useState(false)
+
+  const handleLikeButton = () => {
+    if (!liked) {
+      if (disliked) {
+        setDislikeCounter(dislikeCounter - 1)
+        setDislikecolor('gray')
+        setDisliked(!disliked)
+      }
+      setLikeCounter(likeCounter + 1)
+      setlikecolor('green')
+      setLiked(true)
+    } else {
+      setLikeCounter(likeCounter - 1)
+      setlikecolor('gray')
+      setLiked(!liked)
+    }
+  }
+
+  const handleDislikeButton = () => {
+    if (!disliked) {
+      if (liked) {
+        setLikeCounter(likeCounter - 1)
+        setlikecolor('gray')
+        setLiked(!liked)
+      }
+      setDislikeCounter(dislikeCounter + 1)
+      setDislikecolor('red')
+      setDisliked(!disliked)
+    } else {
+      setDislikeCounter(dislikeCounter - 1)
+      setDislikecolor('gray')
+      setDisliked(!disliked)
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={handleLikeButton}
+        style={{ color: `${likecolor}`, padding: '15px', fontsize: '15px', margin: '15px' }}>
+        Like | {likeCounter}
+      </button>
+      <button
+        onClick={handleDislikeButton}
+        style={{ color: `${dislikecolor}`, padding: '15px', fontsize: '15px', margin: '15px' }}>
+        Dislike | {dislikeCounter}
+      </button>
     </div>
   );
 }
